90b1cdbe7506933659f6ebd617f56d5e6fb0ed10
iEric0228 <118494002+iEric0228@users.noreply.github.com>
2025-12-26 02:05:50 -0500

    Add random suffixes to Lambda resources
    - Add random_string resource for unique naming
    - Update IAM policy name with random suffix
    - Update Lambda function name with random suffix
    - Add source_code_hash for proper Lambda updates
    - This prevents resource name conflicts

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -13,12 +13,19 @@ terraform {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/../../../backend/lambda"  # Your Python code location
-  output_path = "${path.module}/lambda_function.zip"      # Where to create ZIP
+  source_dir  = "${path.module}/../../../backend/lambda"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+resource "random_string" "lambda_suffix" {
+  length  = 6
+  special = false
+  upper   = false
 }
 
 resource "aws_iam_role" "lambda_role" {
   name = "visitor-counter-lambda-role-${var.environment}-${random_string.lambda_suffix.result}"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -33,24 +40,8 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "random_string" "lambda_suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
 resource "aws_iam_policy" "lambda_dynamodb_policy" {
-  name = "visitor-counter-dynamodb-policy-${var.environment}"
+  name = "visitor-counter-dynamodb-policy-${var.environment}-${random_string.lambda_suffix.result}"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -61,23 +52,38 @@ resource "aws_iam_policy" "lambda_dynamodb_policy" {
           "dynamodb:GetItem",
           "dynamodb:UpdateItem"
         ]
-        Resource = var.dynamodb_table_arn # From DynamoDB module
+        Resource = var.dynamodb_table_arn
       }
     ]
   })
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
+}
+
 resource "aws_lambda_function" "visitor_counter" {
-  filename = data.archive_file.lambda_zip.output_path
-  function_name = "visitor-counter-${var.environment}"
-  role = aws_iam_role.lambda_role.arn
-  handler = "handler.lambda_handler"
-  runtime = "python3.11"
+  filename      = data.archive_file.lambda_zip.output_path
+  function_name = "visitor-counter-${var.environment}-${random_string.lambda_suffix.result}"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "handler.lambda_handler"
+  runtime       = "python3.11"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
     variables = {
       TABLE_NAME = var.dynamodb_table_name
+    }
+  }
+
+  tags = {
+    Name        = "VisitorCounterLambda-${var.environment}"
+    Environment = var.environment
   }
 }
-}
-
